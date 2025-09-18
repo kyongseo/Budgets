e0c6e89a357859ef4b7682d2e2e51642559fb5be
kyongseo <pokj930@naver.com>
2025-09-18 14:42:07 +0900

    feat: init commit

--- src/main/java/ks/com/budgetmanagementproject/global/config/SwaggerConfig.java
+++ src/main/java/ks/com/budgetmanagementproject/global/config/SwaggerConfig.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -37,5 +38,18 @@ class SwaggerConfig {
 
         return new OpenAPI()
                 .info(info);
+    }
+
+    @Bean
+    public OpenApiCustomizer applyOperationOrder() {
+        return openApi -> openApi.getPaths().values().forEach(pi ->
+                pi.readOperations().forEach(op -> {
+                    String id = op.getOperationId();
+                    if (id != null) {
+                        try { op.addExtension("x-order", Integer.parseInt(id.split("-")[0])); }
+                        catch (Exception ignored) {}
+                    }
+                })
+        );
     }
 }
