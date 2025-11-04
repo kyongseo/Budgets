8799834ed3a55a748a6df4899dfb2084fb3dacf7
kyongseo <pokj930@naver.com>
2025-11-04 17:34:41 +0900

    feat: kafka - websocket 채팅 설정

--- src/main/java/ks/com/budgetmanagementproject/global/config/WebSocketConfig.java
+++ src/main/java/ks/com/budgetmanagementproject/global/config/WebSocketConfig.java
@@ -1,39 +1,22 @@
 package ks.com.budgetmanagementproject.global.config;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
-@EnableWebSocketMessageBroker
-@AllArgsConstructor
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final ChatWebSocketHandler chatWebSocketHandler;
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        // 스프링의 인메모리 기반 메시지 브로커를 사용한다는 설정
-        // 클라이언트가 메시지를 구독할 때 사용할 접두사를 /sub 설정
-        registry.enableSimpleBroker("/sub");
-
-        // 클라이언트가 메시지를 서버로 보낼 때 사용할 접두사를 /pub 설정
-        registry.setApplicationDestinationPrefixes("/pub");
-
-        // 특정 사용자에게 메시지를 보낼 때 사용할 접두사를 /user 설정
-        registry.setUserDestinationPrefix("/user");
-    }
-
-    /**
-     * 오프닝 핸드셰이크 과정에서 사용할 앤드포인트 지정
-     * ws://localhost:{port}/ws-stomp --> https를 사용하면 wss
-     */
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // WebSocket 연결 주소 (프론트에서 SockJS가 여길 호출함)
-        registry.addEndpoint("/ws-stomp")
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(chatWebSocketHandler, "/chat/**")
+                .setAllowedOrigins("*")
+                .setAllowedOriginPatterns("*");
     }
 }
