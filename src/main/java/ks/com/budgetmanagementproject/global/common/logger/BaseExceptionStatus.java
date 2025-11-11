44d7064054f2912551f76e0da5fc40d9ecd3b249
kyongseo <pokj930@naver.com>
2025-11-11 16:50:47 +0900

    feat: 요청/응답 값 정의

--- src/main/java/ks/com/budgetmanagementproject/global/common/logger/BaseExceptionStatus.java
+++ src/main/java/ks/com/budgetmanagementproject/global/common/logger/BaseExceptionStatus.java
@@ -16,7 +16,12 @@ public enum BaseExceptionStatus {
     NON_EXISTENT_BUDGET(HttpStatus.NOT_FOUND, "존재하지 않는 예산입니다."),
     FORBIDDEN_USER(HttpStatus.FORBIDDEN, "권한이 없는 유저입니다."),
     NON_EXISTENT_EXPENDITURE(HttpStatus.NOT_FOUND, "존재하지 않는 지출입니다."),
-    NON_EXISTENT_TOKEN(HttpStatus.NOT_FOUND, "존재하지 않는 토큰입니다.");
+    NON_EXISTENT_TOKEN(HttpStatus.NOT_FOUND, "존재하지 않는 토큰입니다."),
+
+    CHATROOM_NOT_FOUND(HttpStatus.NOT_FOUND,  "존재하지 않는 채팅방입니다."),
+    CHATROOM_ALREADY_JOINED(HttpStatus.BAD_REQUEST,  "이미 채팅방에 참여 중입니다."),
+    CHATROOM_NOT_MEMBER(HttpStatus.BAD_REQUEST, "채팅방에 참여 중이 아닙니다."),
+    CHATROOM_NAME_REQUIRED(HttpStatus.BAD_REQUEST, "채팅방 이름은 필수입니다.");
 
     private final HttpStatus code;
     private final String message;
