aac2e77eb262de83dd4012dfe4248a93325d36fd
kyongseo <pokj930@naver.com>
2026-03-23 10:06:16 +0900

    ref: 불필요한 패키지 제거

--- src/test/java/ks/com/budgetmanagementproject/feature/budget/service/BudgetServiceTest.java
+++ src/test/java/ks/com/budgetmanagementproject/feature/budget/service/BudgetServiceTest.java
@@ -37,6 +37,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.awaitility.Awaitility.given;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
